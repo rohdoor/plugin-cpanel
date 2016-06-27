528c994652577efb84c52475690a2e8684ad5b0b
rohdoor <rohdoor@gmail.com>
2016-06-27 14:37:35 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,9 +5,9 @@ read value
 cd /usr/local/cpanel/whostmgr/docroot/cgi
 mkdir promgr
 cd promgr
-wget http://galaxyhostinglive.org/community/protection-manager/promgr.conf
-wget http://galaxyhostinglive.org/community/protection-manager/donate.png
-wget -O index.php http://galaxyhostinglive.org/community/protection-manager/index.txt
+wget https://raw.githubusercontent.com/rohdoor/plugin-cpanel/master/promgr.conf
+wget https://raw.githubusercontent.com/rohdoor/plugin-cpanel/master/donate.png
+wget -O index.php https://raw.githubusercontent.com/rohdoor/plugin-cpanel/master/index.txt
 cd
 /usr/local/cpanel/bin/register_appconfig /usr/local/cpanel/whostmgr/docroot/cgi/promgr/promgr.conf
 clear
